48313120cc60b79a1378cbd5d7f938f7261f4628
Andy Seaborne <andy.seaborne@telicent.io>
2023-03-06 14:11:19 +0000

    Prepare for 0.5.0 release

--- fuseki-main.bat
+++ fuseki-main.bat
@@ -1,5 +1,5 @@
 @echo off
 @rem Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
 
-java -cp jena-fuseki-server-4.5.0.jar:lib/* org.apache.jena.fuseki.main.cmds.FusekiMainCmd %*
+java -cp jena-fuseki-server-4.7.0.jar:lib/* org.apache.jena.fuseki.main.cmds.FusekiMainCmd %*
 exit /B
